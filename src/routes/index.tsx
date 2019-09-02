e84ef0b831cfe228e023a3c7741df74732a9ee9f
Yash Sharma <yashsharma205@gmail.com>
2019-09-02 22:21:26 +0400

    Add ErrorBoundry component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ErrorBoundary from '../components/ErrorBoundry';
 
 const Home = lazy(() => import('../components/Home'));
 const About = lazy(() => import('../components/About'));
@@ -8,12 +9,14 @@ export const AppRouter = () => {
   return (
     <Router>
       <React.Fragment>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} />
+            </Switch>
+          </Suspense>
+        </ErrorBoundary>
       </React.Fragment>
     </Router>
   );
